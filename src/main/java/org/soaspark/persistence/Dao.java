ca80d606f5a644f6fe6d5cf7912fdcceb8bca4bc
thiago-michelini <tigomm@gmail.com>
2016-07-04 22:47:51 -0400

    commitando

---
+++ src/main/java/org/soaspark/persistence/Dao.java
@@ -0,0 +1,9 @@
+package org.soaspark.persistence;
+
+public class Dao {
+
+	public void getEntityManager() {
+		System.out.println("optendo o EntityManager");
+	}
+	
+}
--- src/main/java/org/soaspark/services/ClienteService.java
+++ src/main/java/org/soaspark/services/ClienteService.java
@@ -2,14 +2,23 @@ package org.soaspark.services;
 
 import java.math.BigDecimal;
 
+import javax.inject.Inject;
+
+import org.soaspark.persistence.Dao;
+
 public class ClienteService {
+	
+	@Inject
+	private Dao em;
 
 	public <T> void gravar(T entidade) {
+		em.getEntityManager();
 		System.out.println("gravando... " + entidade.getClass().getName());
 	}
 	
 	@SuppressWarnings("unchecked")
 	public <T> T findById(Long id) {
+		em.getEntityManager();
 		System.out.println("find... id->" + id);
 		return (T) new BigDecimal(id.intValue());
 	}
